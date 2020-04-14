c61071fbaff5d9fbdefc0aef31c461331379490b
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-04-14 20:28:41 -0300

    chore: criando user e relacionameto com appointment

--- fase 2/node/src/database/migrations/1586900008112-CreateUsers.ts
+++ fase 2/node/src/database/migrations/1586900008112-CreateUsers.ts
@@ -1,28 +1,33 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1586889834721
-  implements MigrationInterface {
+export default class CreateUsers1586900008112 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'password',
+            type: 'varchar',
             isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
           },
           {
             name: 'created_at',
@@ -40,6 +45,6 @@ export default class CreateAppointments1586889834721
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
