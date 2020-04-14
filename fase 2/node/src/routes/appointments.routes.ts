61014b284d8df3efd741fe3602c874a4ffe316db
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-04-14 17:02:15 -0300

    chore: configurando tudo do typeorm e removendo cod antigo

---
+++ fase 2/node/src/routes/appointments.routes.ts
@@ -0,0 +1,35 @@
+import { Router } from 'express';
+
+import { getCustomRepository } from 'typeorm';
+
+import { parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
+
+const appointmentsRouter = Router();
+
+appointmentsRouter.get('/', async (_, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentRepository);
+  const appointments = await appointmentsRepository.find();
+
+  return res.json(appointments);
+});
+
+appointmentsRouter.post('/', async (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parseDate = parseISO(date);
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
+      provider,
+      date: parseDate,
+    });
+    return res.json(appointment);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+});
+export default appointmentsRouter;
