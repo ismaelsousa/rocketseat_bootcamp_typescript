079bd2e06ff2b844da2e60cab98005ac7bd0d080
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-04-17 10:46:38 -0300

    chore: upload de arquivos

--- fase 2/node/src/routes/users.routes.ts
+++ fase 2/node/src/routes/users.routes.ts
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import ensureAuth from '../middlewares/ensureAuth';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -13,4 +17,13 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuth,
+  upload.single('avatar'),
+  async (req, res) => {
+    return res.send('a');
+  },
+);
 export default usersRouter;
