78324c88c05c899ae657df4f7472f5cfc64b2234
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-04-22 09:11:49 -0300

    Crinando componentes button e input

--- Gobarber/gobarber-web/src/pages/SignIn/index.tsx
+++ Gobarber/gobarber-web/src/pages/SignIn/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import Logo from '../../assets/logo.svg'
 
-import {FiLogIn} from 'react-icons/fi'
+import {FiLogIn,FiMail, FiLock} from 'react-icons/fi'
 import {Container, Content, Background} from './styles'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
 const SignIn:React.FC = () => {
 	return <Container>
 		<Content>
 			<img src={Logo} alt="logo do gobarber"/>
 			<form >
 				<h1>Faça seu logon</h1>
-				<input type="email" placeholder="email" />
-				<input type="password" placeholder="senha"/>
-				<button type="submit">Entrar</button>
+				<Input type="email" icon={FiMail} name='email' placeholder="email" />
+				<Input type="password"  icon={FiLock} name='password'  placeholder="senha"/>
+				<Button type="submit">Entrar</Button>
 				<a href="fogot">Esqueci minha senha!</a>
 			</form>
 			<a href="">
--- Gobarber/gobarber-web/src/pages/SignIn/styles.ts
+++ Gobarber/gobarber-web/src/pages/SignIn/styles.ts
@@ -41,33 +41,9 @@ export const Content = styled.div`
 
 		}
 
-		input{
-			background:#232129;
-			border-radius:10px;
-			border: 2px solid #232129;
-			padding:16px;
-			width:100%;
-			color:#f4ede8;
-			& + input{
-				margin-top:8px;
-			}
-		}
+		
 
-		button{
-			background:#ff9000;
-			height: 56px;
-			border-radius:10px;
-			border: 0;
-			padding: 0 16px;
-			color:#312e38;
-			width:100%;
-			font-weight:500;	
-			margin-top:16px;
-			transition: background-color 2s;
-			&:hover{
-				background: ${shade(0.2, "#ff9000")}
-			}
-		}
+
 
 		a{
 			color:#f4ede8;
@@ -81,6 +57,7 @@ export const Content = styled.div`
 		}
 	}
 `;
+
 export const Background = styled.div`
 	flex:1;
 	background: url(${background}) no-repeat center;
