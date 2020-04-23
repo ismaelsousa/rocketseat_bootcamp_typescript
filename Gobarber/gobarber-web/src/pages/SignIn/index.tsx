1e860aa9079cd795a301829067babf16d02ffffe
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-04-23 20:21:25 -0300

    chore: validação di sign

--- Gobarber/gobarber-web/src/pages/SignIn/index.tsx
+++ Gobarber/gobarber-web/src/pages/SignIn/index.tsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import React,{useRef, useCallback} from 'react'
 import Logo from '../../assets/logo.svg'
-
+import {Form} from '@unform/web'
+import {FormHandles} from "@unform/core"
+import * as Yup from 'yup'
+import getValidationErrors from '../../utils/getValidationErrors'
 import {FiLogIn,FiMail, FiLock} from 'react-icons/fi'
 import {Container, Content, Background} from './styles'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 const SignIn:React.FC = () => {
+
+	const formRef = useRef<FormHandles>(null)
+
+	const handleSubmit = useCallback(async (data:object) =>{
+		try {
+			formRef.current?.setErrors({})
+			const schema = Yup.object().shape({
+				email:Yup.string().required('Email obrigatório').email('Digite um email válido'),
+				password:Yup.string().required('Senha obrigatória')
+			})
+			
+			await schema.validate(data,{
+				abortEarly:false
+			})
+		} catch (error) {
+			
+			const errors = getValidationErrors(error)
+			formRef.current?.setErrors(errors)
+			
+		}
+	},[])
+
 	return <Container>
 		<Content>
 			<img src={Logo} alt="logo do gobarber"/>
-			<form >
+			<Form ref={formRef} onSubmit={handleSubmit} >
 				<h1>Faça seu logon</h1>
 				<Input type="email" icon={FiMail} name='email' placeholder="email" />
 				<Input type="password"  icon={FiLock} name='password'  placeholder="senha"/>
 				<Button type="submit">Entrar</Button>
 				<a href="fogot">Esqueci minha senha!</a>
-			</form>
+			</Form>
 			<a href="">
 			<FiLogIn/>
 			Criar conta
