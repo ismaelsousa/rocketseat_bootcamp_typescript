cabae212e9271fdb05dcdb2fcd31448d80acb3c1
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-04-25 11:25:42 -0300

    fix: nos styles

--- Gobarber/gobarber-web/src/components/Input/styles.ts
+++ Gobarber/gobarber-web/src/components/Input/styles.ts
@@ -1,9 +1,10 @@
-import styled, {css} from 'styled-components';
-import Tooltip from '../Tooltip'
-interface ContainerPros{
-	isFocused:boolean
-	isField:boolean
-	isErrored:boolean
+import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip';
+
+interface ContainerPros {
+	isFocused: boolean;
+	isField: boolean;
+	isErrored: boolean;
 }
 export const Container = styled.div<ContainerPros>`
 
@@ -20,18 +21,24 @@ export const Container = styled.div<ContainerPros>`
 	& + div{
 				margin-top:8px;
 	}
-	${props => props.isErrored && css`
-		border-color:#c53030;
-	`}
+	${(props) =>
+		props.isErrored &&
+		css`
+			border-color: #c53030;
+		`}
 
-	${props => props.isFocused && css`
-		color:#ff9000;
-		border-color:#ff9000;
-	`}
+	${(props) =>
+		props.isFocused &&
+		css`
+			color: #ff9000;
+			border-color: #ff9000;
+		`}
 	
-	${props => props.isField && css`
-		color:#ff9000;
-	`}
+	${(props) =>
+		props.isField &&
+		css`
+			color: #ff9000;
+		`}
 	
 
 	input{
@@ -47,17 +54,17 @@ export const Container = styled.div<ContainerPros>`
 `;
 
 export const Error = styled(Tooltip)`
-	height:20px;
-	margin-left:-16px;
-	svg{
-		margin:0;
+	height: 20px;
+	margin-left: -16px;
+	svg {
+		margin: 0;
 	}
 
-	span{
-		background:#c53030;
-		color:#f4ede8;
-		&::before{
-				border-color:#c53030 transparent;
+	span {
+		background: #c53030;
+		color: #f4ede8;
+		&::before {
+			border-color: #c53030 transparent;
 		}
 	}
-`
+`;
--- Gobarber/gobarber-web/src/components/ToastContainer/index.tsx
+++ Gobarber/gobarber-web/src/components/ToastContainer/index.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import {useTransition} from 	'react-spring'
+import { useTransition } from 'react-spring';
 import { Container } from './styles';
-import {ToastMessage} from '../../hooks/ToastContext'
-import Toast from './Toast'
+import { ToastMessage } from '../../hooks/ToastContext';
+import Toast from './Toast';
+
 interface ToastContainerProps {
-	messages:ToastMessage[]
+	messages: ToastMessage[];
 }
 
-const  ToastContainer: React.FC<ToastContainerProps> = ({messages})=> {
+const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
 	const messagesWithTransitions = useTransition(
-		messages, 
-		(message)=>message.id,
+		messages,
+		(message) => message.id,
 		{
-			from:{right:'-120%', opacity:0},
-			enter:{right:'0%',opacity:1},
-			leave:{right:'-120%', opacity:0},
-		}
-	)
+			from: { right: '-120%', opacity: 0 },
+			enter: { right: '0%', opacity: 1 },
+			leave: { right: '-120%', opacity: 0 },
+		},
+	);
 	return (
-		<Container >
-			{messagesWithTransitions.map(({item, key, props})=>(
-				<Toast key={key} style={props} message={item}/>		
+		<Container>
+			{messagesWithTransitions.map(({ item, key, props }) => (
+				<Toast key={key} style={props} message={item} />
 			))}
 		</Container>
 	);
-}
+};
 
-
-export default ToastContainer
+export default ToastContainer;
