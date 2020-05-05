482b87edb14b458ec023a422bb7d3bda2feb259d
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-05-04 21:04:59 -0300

    chore: add files of components

--- Gobarber/native/src/pages/SignIn/index.tsx
+++ Gobarber/native/src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 import logo from '../../assets/logo.png';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import { Container, Title } from './styles';
 
 const SignIn: React.FC = () => {
@@ -8,6 +11,11 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logo} />
       <Title>Faça seu logon</Title>
+      <Icon name="mail" />
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
