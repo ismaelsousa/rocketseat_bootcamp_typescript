63a57d4e7eb9212504e1c8b3e4862389e73226e4
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-05-14 10:13:32 -0300

    Add teste de atutenticação do usuario com senha errada e sem usuario

--- Gobarber/node/src/modules/users/services/AuthenticateUserService.test.ts
+++ Gobarber/node/src/modules/users/services/AuthenticateUserService.test.ts
@@ -1,4 +1,5 @@
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import AppError from '@shared/errors/AppError';
 import AuthenticateUserService from './AuthenticateUserService';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
@@ -29,5 +30,49 @@ describe('AuthenticateUserService', () => {
 
     expect(reponse).toHaveProperty('token');
     expect(reponse.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'john@emaple.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const createUserService = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUserService.execute({
+      email: 'john@emaple.com',
+      password: '123456',
+      name: 'john',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'john@emaple.com',
+        password: 'wrong',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
