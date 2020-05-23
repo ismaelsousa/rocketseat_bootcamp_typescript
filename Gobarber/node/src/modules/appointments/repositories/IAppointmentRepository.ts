60eb15888d88efba329c9d0b3cd1cf2cb7fd2ebd
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-05-23 09:06:28 -0300

    Listando dias disponiveis

--- Gobarber/node/src/modules/appointments/repositories/IAppointmentRepository.ts
+++ Gobarber/node/src/modules/appointments/repositories/IAppointmentRepository.ts
@@ -1,9 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
 import ICreateAppointmentDTO from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
-interface IAppointmentsRepository {
+export default interface IAppointmentsRepository {
   create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    date: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
-
-export default IAppointmentsRepository;
--- Gobarber/node/src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
+++ Gobarber/node/src/modules/appointments/repositories/fakes/FakeAppointmentRepository.ts
@@ -1,11 +1,26 @@
 import { uuid } from 'uuidv4';
-import { isEqual } from 'date-fns';
+import { isEqual, getMonth, getYear } from 'date-fns';
 import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentRepository';
 import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '@modules/appointments/dtos/IFindAllInMonthFromProviderDTO';
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
 class AppointmentRepository implements IAppointmentRepository {
   private appointments: Appointment[] = [];
+
+  public async findAllInMonthFromProvider({
+    month,
+    year,
+    provider_id,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      appointment =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year,
+    );
+    return appointments;
+  }
 
   public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = this.appointments.find(appointment =>
