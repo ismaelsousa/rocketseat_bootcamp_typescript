8548ec4d92e09fff094ae2db8d3704f4ec46c46d
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-05-23 09:41:38 -0300

    Excluindo horários antigos

--- Gobarber/node/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ Gobarber/node/src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 import IAppointmentsRepository from '../repositories/IAppointmentRepository';
 
 interface IRequest {
@@ -43,15 +43,18 @@ export default class ListProviderDayAvailabilityService {
       { length: 10 },
       (_, Index) => Index + hourStart,
     );
+    const currentDate = new Date(Date.now());
 
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHour = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
+
       return {
         hour,
-        available: !hasAppointmentInHour,
+        available: !hasAppointmentInHour && isAfter(compareDate, currentDate),
       };
     });
     return availability;
