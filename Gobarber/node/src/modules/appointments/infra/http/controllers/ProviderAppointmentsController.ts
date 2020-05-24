22a437c9adf38372aecd72a742bd152842f9ba26
ismael moreira de sousa <ismaelsousa1998@outlook.com>
2020-05-24 11:21:05 -0300

    Agenda do Prestador, Configurando mongoDB  e Estrutura de notificações

---
+++ Gobarber/node/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { day, month, year } = request.body;
+
+    /**
+     * O container resolver verificar se precisa injetar uma dependencia,
+     * se sim,
+     * ele buscar no container que criei procurando a dependencia
+     */
+
+    const listProviderAppointments = container.resolve(
+      ListProviderAppointmentsService,
+    );
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+    return response.json(appointments);
+  }
+}
---
+++ Gobarber/node/src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata';
+
+import { injectable, inject } from 'tsyringe';
+import IAppointmentsRepository from '../repositories/IAppointmentRepository';
+import Appointment from '../infra/typeorm/entities/Appointment';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+  day: number;
+}
+
+@injectable()
+export default class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
+      {
+        provider_id,
+        month,
+        year,
+        day,
+      },
+    );
+
+    return appointments;
+  }
+}
